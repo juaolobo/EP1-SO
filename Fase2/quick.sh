cf46688fabd1199d3b99cc9e91baed95572e5f94
João Lobo <49628984+juaolobo@users.noreply.github.com>
2020-10-02 21:48:54 -0300

    Update quick.sh

--- Fase2/quick.sh
+++ Fase2/quick.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-DIR=$(pwd)
-ESC=1
-read ESC
-echo $ESC
 for number in {1..30}
 do
-	CWD="./ep1 tests/trace1.txt $ESC output/fcfs/trace1_$number -d"
+	CWD="./ep1 tests/trace1.txt 1 output/fcfs/trace1_$number -d"
 	echo $CWD
 	$CWD
 done
